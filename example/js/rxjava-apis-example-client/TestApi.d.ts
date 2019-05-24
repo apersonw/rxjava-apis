9a63f07e34547b52361a85735879cbbf0319141d
apikit <apikit@rxjava.org>
2019-05-24 17:06:07 +0800

    更新SDK版本:28

---
+++ example/js/rxjava-apis-example-client/TestApi.d.ts
@@ -0,0 +1,30 @@
+import TestForm from './form/TestForm'
+import {AbstractApi} from 'apikit-core'
+
+
+/**
+ * 
+*/
+declare class TestApi extends AbstractApi {
+
+
+
+   /**
+     * 
+    *
+     * <div class='http-info'>http 说明<ul>
+     * <li><b>Uri:</b>person/testeeee/{id}</li>
+     * <li><b>PathVariable:</b> string id</li>
+     * <li><b>Form:</b>TestFormtestewwPath</li>
+     * <li><b>Model:</b> number</li>
+     * <li>需要登录</li>
+     * </ul>
+     * </div>
+     * @see TestForm
+    */
+    testewwPath(id:string, form:TestForm):Promise<number>;
+
+}
+export { TestApi };
+declare const testApi: TestApi;
+export default testApi;
---
+++ example/js/rxjava-apis-example-client/index.d.ts
@@ -0,0 +1,7 @@
+
+import TestApi from '.org/rxjava/api/person/example/TestApi';
+
+
+export {
+    TestApi,
+};

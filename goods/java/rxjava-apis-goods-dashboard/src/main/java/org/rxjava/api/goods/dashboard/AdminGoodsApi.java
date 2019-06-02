65763996542558e96d1c07f0c9464a4094da4d97
apikit <apikit@rxjava.org>
2019-06-02 19:02:32 +0800

    更新SDK版本:76

--- goods/java/rxjava-apis-goods-dashboard/src/main/java/org/rxjava/api/goods/dashboard/AdminGoodsApi.java
+++ goods/java/rxjava-apis-goods-dashboard/src/main/java/org/rxjava/api/goods/dashboard/AdminGoodsApi.java
@@ -22,19 +22,19 @@ public class AdminGoodsApi {
 		this.clientAdapter = clientAdapter;
 	}
 
-	public Mono<Goods> create() {
-		Map<String, Object> _uriVariables = new HashMap<>();
-		String _url = ApiUtils.expandUriComponent("admin/goods", _uriVariables);
-
-		return clientAdapter.request("POST", _url, null, _0Type);
-	}
-
 	public Mono<Page<Goods>> getPage(GoodsPageForm form) {
 		Map<String, Object> _uriVariables = new HashMap<>();
 		String _url = ApiUtils.expandUriComponent("admin/goodsPage", _uriVariables);
 
 		List<Entry<String, Object>> _form = form.encode("", new ArrayList<>());
-		return clientAdapter.request("GET", _url, _form, _1Type);
+		return clientAdapter.request("GET", _url, _form, _0Type);
+	}
+
+	public Mono<Goods> save() {
+		Map<String, Object> _uriVariables = new HashMap<>();
+		String _url = ApiUtils.expandUriComponent("admin/goods", _uriVariables);
+
+		return clientAdapter.request("POST", _url, null, _1Type);
 	}
 
 	public void setclientAdapter(ClientAdapter clientAdapter) {
@@ -45,6 +45,6 @@ public class AdminGoodsApi {
 		return clientAdapter;
 	}
 
-	private static final ApiType _0Type = ApiUtils.type(Goods.class);
-	private static final ApiType _1Type = ApiUtils.type(Page.class, ApiUtils.type(Goods.class));
+	private static final ApiType _0Type = ApiUtils.type(Page.class, ApiUtils.type(Goods.class));
+	private static final ApiType _1Type = ApiUtils.type(Goods.class);
 }

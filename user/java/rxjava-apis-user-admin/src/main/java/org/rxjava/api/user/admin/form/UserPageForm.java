97c8d83d5f9a04529fd379b38fb7ad72e87f9a3b
apikit <apikit@rxjava.org>
2019-06-07 03:55:19 +0800

    更新SDK版本:84

---
+++ user/java/rxjava-apis-user-admin/src/main/java/org/rxjava/api/user/admin/form/UserPageForm.java
@@ -0,0 +1,16 @@
+package org.rxjava.api.user.admin.form;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+@Getter
+@Setter
+public class UserPageForm extends PageForm {
+
+	public List<Entry<String, Object>> encode(String $parent, List<Entry<String, Object>> $list) {
+		return $list;
+	}
+}

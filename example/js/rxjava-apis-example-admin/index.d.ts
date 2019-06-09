5f99e9acdb56d4686d633636b756fdee11ac9e85
apikit <apikit@rxjava.org>
2019-06-10 02:39:41 +0800

    更新SDK版本:90

---
+++ example/js/rxjava-apis-example-admin/index.d.ts
@@ -0,0 +1,7 @@
+
+import AdminTestApi from './AdminTestApi';
+
+
+export {
+    AdminTestApi,
+};
---
+++ example/js/rxjava-apis-example-admin/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+Object.defineProperty(exports, "AdminTestApi", {
+        enumerable: true,
+        get: function get() {
+        return _AdminTestApi.default;
+    }
+});
+var _AdminTestApi = _interopRequireDefault(require("./AdminTestApi"));
+
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }

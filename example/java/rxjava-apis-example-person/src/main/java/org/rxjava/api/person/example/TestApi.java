83d2cd1978e17d8c24b1baabfee1b0b898b7d9d6
apikit <apikit@rxjava.org>
2019-06-29 23:31:40 +0800

    更新SDK版本:125

--- example/java/rxjava-apis-example-person/src/main/java/org/rxjava/api/person/example/TestApi.java
+++ example/java/rxjava-apis-example-person/src/main/java/org/rxjava/api/person/example/TestApi.java
@@ -20,13 +20,20 @@ public class TestApi {
 		this.clientAdapter = clientAdapter;
 	}
 
+	public Mono<String> checkTest() {
+		Map<String, Object> _uriVariables = new HashMap<>();
+		String _url = ApiUtils.expandUriComponent("person/checkTest", _uriVariables);
+
+		return clientAdapter.request("GET", _url, null, _0Type);
+	}
+
 	public Mono<Integer> testPath(String id, TestForm form) {
 		Map<String, Object> _uriVariables = new HashMap<>();
 		_uriVariables.put("id", id);
 		String _url = ApiUtils.expandUriComponent("person/testPath/{id}", _uriVariables);
 
 		List<Entry<String, Object>> _form = form.encode("", new ArrayList<>());
-		return clientAdapter.request("GET", _url, _form, _0Type);
+		return clientAdapter.request("GET", _url, _form, _1Type);
 	}
 
 	public void setclientAdapter(ClientAdapter clientAdapter) {
@@ -37,5 +44,6 @@ public class TestApi {
 		return clientAdapter;
 	}
 
-	private static final ApiType _0Type = ApiUtils.type(Integer.class);
+	private static final ApiType _0Type = ApiUtils.type(String.class);
+	private static final ApiType _1Type = ApiUtils.type(Integer.class);
 }
